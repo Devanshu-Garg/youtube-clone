624c75f7d01db8dda500c3c51b9df7ade748bb86
Devanshu-Garg <devanshugarg2608@gmail.com>
2023-10-31 07:45:09 +0530

    got add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-
+import React from 'react';
 import './App.css';
-
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-    <h1>JAI SHREE RAM</h1>
+    {/* <h1>JAI SHREE RAM</h1> */}
+    <Header/>
+    {/* Sidebar
+    RecommendedVideo */} 
     </div>
   );
 }
